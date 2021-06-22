d7123552b74ec0235c7fd8843f66b183b55e308a
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-06-22 16:54:24 -0500

    cleaned up in line JSX output code through a let variable

--- src/App.js
+++ src/App.js
@@ -35,16 +35,31 @@ function App() {
     setIsLoading(false);
   }
 
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {content}
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {!isLoading && movies.length === 0 && !error && <p>Found no movies.</p>}
         {!isLoading && error && <p>{error}</p>}
-        {isLoading && <p>Loading...</p>}
+        {isLoading && <p>Loading...</p>} */}
       </section>
     </React.Fragment>
   );
