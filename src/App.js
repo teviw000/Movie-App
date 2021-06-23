a8e61873a42ac058a120c3f28ede63b5d561301a
teviw000 <31836504+teviw000@users.noreply.github.com>
2021-06-23 16:43:45 -0500

    Added new AddMovie.js file which will handle form submission of new movie data. Created two way communication to firebase app with our current movie app.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from './components/AddMovie';
 import "./App.css";
 
 function App() {
@@ -12,23 +13,33 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/"); // returns a promise
+      const response = await fetch("https://react-http-c2d5e-default-rtdb.firebaseio.com/movies.json"); // returns a promise
       if (!response.ok) {
         throw new Error("Something went wrong!"); //throw this error if we have unsuccessful response
       }
 
       const data = await response.json(); //do the transformation for JSON body to a JS object
 
-      // still async
-      const transformMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          releaseDate: movieData.release_date,
-          openingText: movieData.opening_crawl,
-        };
-      });
-      setMovies(transformMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          releaseData: data[key].releaseDate,
+          openingText: data[key].openingText
+        })
+      }
+      // still async, not needed anymore since we use the code above
+      // const transformMovies = data.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     releaseDate: movieData.release_date,
+      //     openingText: movieData.opening_crawl,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message); //error is now a string from the throw block
     }
@@ -38,6 +49,18 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-c2d5e-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST', // default is GET
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }); // can be used to fetch and send data
+    const data = await response.json();
+    console.log(data);
+  }
 
   let content = <p>Found no movies.</p>;
 
@@ -55,6 +78,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
